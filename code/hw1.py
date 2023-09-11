a19df2e0ec8e627fc83f8da54e8391ffcfb54789
alimostafavi24 <119982603+alimostafavi24@users.noreply.github.com>
2023-09-10 17:24:51 -0700

    commit pdf

--- code/hw1.py
+++ code/hw1.py
@@ -26,67 +26,68 @@ PATH_TO_W_DATA = os.path.join(DATA_ROOT, 'w.txt')
 
 FIGURES_ROOT = '../figures'
 PATH_TO_WALK_FIGURE = os.path.join(FIGURES_ROOT, 'walk.png')
-walk_arr = numpy.loadtxt(PATH_TO_WALK_DATA)
 # -----------------------------------------------------------------------------
 def exercise_6(path_to_data, path_to_figure):
-
+    """
+    Input: path to load the data and save the figure
+    The function is used to load the data, plot the data, finding its minimum and maximum value
+    and finally linear scaling of the data.
+    """
     print('='*30)
     print('Running exercise_6()')
 
     #### YOUR CODE HERE ####
-    walk_arr = numpy.loadtxt(path_to_data)
+    walk_arr = numpy.loadtxt(path_to_data) #loading the data from the proper directory
 
     #### YOUR CODE HERE ####
     # plot the data using matplotlib plot!
     print(f'walk_arr.shape: {walk_arr.shape}')
 
-    plt.figure(figsize=(10,6))
+    plt.figure() #plotiing the data with mathplotlib
 
-    plt.plot(walk_arr,label='Random walk' , linewidth = 3, color='black')
+    plt.plot(walk_arr,label='Random walk' , linewidth = 3, color='black') #plotiing the data with mathplotlib
 
     ##create labels
-    plt.xlabel("Step")
-    plt.ylabel("Location")
-    plt.title("Random walk")
-    plt.legend(["walk_arr"])
+    plt.xlabel("Step") #xlabel
+    plt.ylabel("Location") #ylabel
+    plt.title("Random Walk") #title
+    plt.legend(["walk_arr"]) #legend
 
     #save the figure
     plt.savefig(path_to_figure, format='png')
 
     #### YOUR CODE HERE ####
-    walk_min = numpy.min(walk_arr)
+    walk_min = numpy.min(walk_arr) #finding the minimum
 
     print(f'walk_min: {walk_min}')
 
     #### YOUR CODE HERE ####
-    walk_max = numpy.max(walk_arr)
+    walk_max = numpy.max(walk_arr) #finding the maximum
 
     print(f'walk_max: {walk_max}')
 
-    #### YOUR CODE HERE ####
+    #### YOUR CODE HERE #### 
+    #### linear scaing function which gets raw data, maximum and minimum values
+    #### and returns scaled data.
+
     def linear_scaling(data, new_max, new_min):
+        """
+        Linearly scale the values of an array in the range [new_max,new_min]
+        :data, new_max, new_min: input ndarray
+        :return: linear scaling (linearly scaled data)
+        """
 
-        old_max = numpy.max(walk_arr)
-        old_min = numpy.min(walk_arr)
+        old_max = numpy.max(data) #min
+        old_min = numpy.min(data) # max
 
-        linear_scaling =  (data - old_min) / (old_max - old_min) * (new_min - new_max) + new_max
+        linear_scaling =  (data - old_min) / (old_max - old_min) * (new_min - new_max) + new_max 
+        #the above equation is used for linear scaling
     
         return linear_scaling 
 
 
-    walk_arr_scaled = linear_scaling(walk_arr, -2, +3)
+    walk_arr_scaled = linear_scaling(walk_arr, -2, +3) #scaled data
 
-    plt.figure(figsize=(10,6))
-
-    plt.plot(walk_arr_scaled,label='Random walk' , linewidth = 3)
-
-    ##create labels
-    plt.xlabel("Step")
-    plt.ylabel("Location")
-    plt.title("Random walk")
-    plt.legend(["walk_arr_scaled"])
-
-    print('DONE exercise_6()')
 
     return walk_arr, walk_min, walk_max, walk_arr_scaled
 
@@ -95,7 +96,11 @@ def exercise_6(path_to_data, path_to_figure):
 # -----------------------------------------------------------------------------
 
 def exercise_7():
-
+    """
+    This function generates a deterministic sequence of values. It would be modified to gernerate random values based on
+    number of thrown dices, numpy random seed and number of trials. Reuslts would be compared against each other at the 
+    final step for the verification of the controlled random number generation.
+    """
     print('=' * 30)
     print('Running exercise_7()')
 
@@ -121,11 +126,20 @@ def exercise_7():
             # of the probability of throwing 'doubles' with two fair six-sided dice
             # (i.e., the probability that the dice end up with teh same values)
             # based on throwing the two dice num_dice_throws times.
-            dice_1 = numpy.random.randint(1, 7, num_dice_throws)
-            dice_2 = numpy.random.randint(1, 7, num_dice_throws)
-            dice_doubles = numpy.sum(dice_1 == dice_2)
+            # initailization
+            dice_1 = numpy.zeros(num_dice_throws, dtype=int)
+            dice_2 = numpy.zeros(num_dice_throws, dtype=int)
+            dice_doubles = 0 #number of times dice_1 and dice_2 have equal values
+            
+            #here we calculate how many times dice_1 and dice_2 end up with the same values
+            for i in range (num_dice_throws):
+                dice_1[i] = numpy.random.randint(6) #1st dice (generated random numbers are between 0 and 5)
+                dice_2[i] = numpy.random.randint(6) #2nd dice (generated random numbers are between 0 and 5)
+                if dice_1[i] == dice_2[i]:
+                   dice_doubles += 1
+                           
 
-            probability_estimate = dice_doubles / (num_dice_throws)
+            probability_estimate = dice_doubles/ (num_dice_throws) #probbailty o the double dice=dice_dobles/total dice throws
 
             # Save the probability estimate for each trial (you don't need to change
             # this next line)
@@ -149,7 +163,7 @@ def exercise_7():
     numpy.random.seed(7)
 
 
-    experiment_outcomes_3 = run_experiment()
+    experiment_outcomes_3 = run_experiment() #Here we should get the same values as experiment 1
 
     print(f'experiment_outcomes_3: {experiment_outcomes_3}')
 
@@ -161,6 +175,10 @@ def exercise_7():
 # -----------------------------------------------------------------------------
 
 def exercise_8():
+    """
+    This function is defined to create random matrices and perform algebra operators such as
+    summation, multiplication dot product, as well as finding the transpose and inverse of a matrix.
+    """
 
     print("=" * 30)
     print("Running exercise_8()")
@@ -168,7 +186,7 @@ def exercise_8():
     #### YOUR CODE HERE ####
     # set the numpy random seed to 7
 
-    numpy.random.seed(7)
+    numpy.random.seed(7)    # set the numpy random seed equal to 7
 
     #### YOUR CODE HERE ####
     # Set x to a 2-d array of random number of shape (3, 1)
@@ -178,7 +196,7 @@ def exercise_8():
 
     #### YOUR CODE HERE ####
     # Set 7 to a 2-d array of random number of shape (3, 1)
-    y = numpy.random.rand(3,1)
+    y = numpy.random.rand(3,1) # Repeat the previous step
 
     print(f'y:\n{y}')
 
@@ -226,13 +244,13 @@ def exercise_8():
 
     #### YOUR CODE HERE ####
     # Compute the inverse of A
-    v6 = numpy.linalg.inv(A)
+    v6 = numpy.linalg.inv(A) #linalg.inv calculates the inverse of A
     print(f'v6:\n{v6}')
 
     #### YOUR CODE HERE ####
     # Compute the dot product of A with its inverse.
     #   Should be near identity (save for some numerical error)
-    v7 = numpy.dot(A, v6)
+    v7 = numpy.dot(A, v6) #This should be equal to the identity matrix
 
     print(f'v7:\n{v7}')
 
@@ -242,22 +260,29 @@ def exercise_8():
 # -----------------------------------------------------------------------------
 
 def exercise_9(path_to_X_data, path_to_w_data):
+    """
+    This function aids in verifying the exercise 3.
+    Input:
+    - path_to_X_data, path_to_w_data: path to the input data
 
+    Returns:
+    - type: values for right hand side and left hand side of equation 3.
+    """
     print("="*30)
     print("Running exercise_9()")
 
     #### YOUR CODE HERE ####
     # load the X and w data from file into arrays
-    X = numpy.loadtxt(path_to_X_data, delimiter = ',')
-    w = numpy.loadtxt(path_to_w_data)
+    X = numpy.loadtxt(path_to_X_data, delimiter = ',') #X dara with specofoed delimiter
+    w = numpy.loadtxt(path_to_w_data) #W data
 
     print(f'X:\n{X}')
     print(f'w: {w}')
 
     #### YOUR CODE HERE ####
     # Extract the column 0 (x_n1) and column 1 (x_n2) vectors from X
-    x_n1 = X[:, 0]
-    x_n2 = X[:, 1]
+    x_n1 = X[:, 0] #extracting the first column
+    x_n2 = X[:, 1] #extracting the second column
 
     print(f'x_n1: {x_n1}')
     print(f'x_n2: {x_n2}')
@@ -267,10 +292,11 @@ def exercise_9(path_to_X_data, path_to_w_data):
     #   (Exercise 1.3 from FCMA p.35)
     # Set the final value to
 
-    RHS = 0
-    N = numpy.size(x_n1)
+    RHS = 0 #setting the right hand side of rexercise 3 to 0. Then do the summation with scalar arithmetic to find the total summation.
+    N = numpy.size(x_n1) #finding the size of x_n1 and use it for for loop
 
     for i in range(N):
+        #Here the summation is performed and RHS is equal to the sum and stored into scalar_result.
         RHS = RHS + (w[0] ** 2) * (x_n1[i] ** 2) + \
         (2 * w[0] * w[1]) * x_n1[i] * x_n2[i] + \
         (w[1] ** 2) * (x_n2[i] ** 2)
@@ -288,14 +314,14 @@ def exercise_9(path_to_X_data, path_to_w_data):
     # first computer the inner term: X-transpose times X (XX), and then
     # below you complete the computation by multiplying on the left and right
     # by w (wXXw)
-    XX = numpy.dot(X.T , X)
+    XX = numpy.dot(X.T , X) #First calculate the XX=XTX
 
     print(f'XX:\n{XX}')
 
     #### YOUR CODE HERE ####
     # Now you'll complete the computation by multiplying on the left and right
     # by w to determine the final value: wXXw
-    wXXw = numpy.dot(numpy.dot(w.T , XX),w)
+    wXXw = numpy.dot(numpy.dot(w.T , XX),w) #calculate wT(XTX)w
 
     print(f'wXXw: {wXXw}')
 
